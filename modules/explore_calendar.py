528f460e9f9227528e65ec26eb46dd482903b230
garthee <garthee@dhcp-18-111-92-90.dyn.mit.edu>
2013-09-21 08:38:53 -0400

    Updated with comments

--- modules/explore_calendar.py
+++ modules/explore_calendar.py
@@ -4,25 +4,30 @@ from jinja2 import Markup
 
 def render(vis, request, info):
 	info["message"] = [] 
+	
+	# module independent user inputs
 	table = request.args.get("table", '')
-	xField = request.args.get("xField", '')
-	field = request.args.get("field", '') 
 	where = request.args.get("where", '1=1')
 	limit = request.args.get("limit", '3650') # 10 years max
 	start = request.args.get("start", '0') # start at 0
-		
 	reload = int(request.args.get("reload", 0))
 	view = request.args.get("view", '')
+	
+	#module dependent user inputs
+	xField = request.args.get("xField", '')
+	field = request.args.get("field", '')
 
+	# verify essential parameter details - smell test
 	if len(table) == 0 or len(xField) == 0:
 		info["message"].append("Table or xField missing")
 		info["message_class"] = "failure"
 	else:
+		# prepare sql query
 		sql = "select %s, %s from %s where %s group by 1 order by 1 limit %s offset %s"%(xField, field, table, where,limit, start)
 
 		header =  "Date,Field"
-		
 		(datfile, reload, result) = export_sql(sql, vis.config, reload, header, view)
+		
 		if len(result) > 0:
 			info["message"].append(result)
 			info["message_class"] = "failure"
@@ -33,11 +38,15 @@ def render(vis, request, info):
 			else:
 				info["message"].append("Loading from cache. Use reload=1 to reload.")
 			
+			# if success return data file
 			info["datfile"] = datfile
 
+	# prepare some messages
 	field = ','.join([re.compile(r'as').split(f)[-1].strip() for f in field.split(',')])
 	info["message"] = Markup(''.join('<p>%s</p>'%m for m in info["message"] if len(m) > 0))
 	info["title"] = "%s from %s"%(field, table)
+	
+	# format the message to encode HTML characters
 	info['query']= Markup(request.args.get('query', ''))
 	
 
